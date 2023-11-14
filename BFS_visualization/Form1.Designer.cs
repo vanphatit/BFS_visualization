d75fcad738b38ceee819c70cefefe4b444948a53
Phat Lee <vanphat15it@gmail.com>
2023-11-14 22:38:34 +0700

    UI + DrawTree

--- BFS_visualization/Form1.Designer.cs
+++ BFS_visualization/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.pnlGraph1 = new System.Windows.Forms.Panel();
+            this.button10 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
@@ -38,7 +39,6 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.btn1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.cbMatrixOrder = new System.Windows.Forms.ComboBox();
@@ -55,6 +55,7 @@
             // pnlGraph1
             // 
             this.pnlGraph1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pnlGraph1.Controls.Add(this.button10);
             this.pnlGraph1.Controls.Add(this.button9);
             this.pnlGraph1.Controls.Add(this.button8);
             this.pnlGraph1.Controls.Add(this.button7);
@@ -64,118 +65,107 @@
             this.pnlGraph1.Controls.Add(this.button3);
             this.pnlGraph1.Controls.Add(this.button2);
             this.pnlGraph1.Controls.Add(this.button1);
-            this.pnlGraph1.Controls.Add(this.btn1);
             this.pnlGraph1.Location = new System.Drawing.Point(12, 12);
             this.pnlGraph1.Name = "pnlGraph1";
-            this.pnlGraph1.Size = new System.Drawing.Size(642, 529);
+            this.pnlGraph1.Size = new System.Drawing.Size(859, 597);
             this.pnlGraph1.TabIndex = 0;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(95, 217);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(42, 43);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "1";
+            this.button10.UseVisualStyleBackColor = true;
             // 
             // button9
             // 
-            this.button9.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.Location = new System.Drawing.Point(67, 276);
+            this.button9.Location = new System.Drawing.Point(115, 346);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(42, 38);
-            this.button9.TabIndex = 9;
-            this.button9.Text = "9";
+            this.button9.Size = new System.Drawing.Size(42, 43);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "1";
             this.button9.UseVisualStyleBackColor = true;
             // 
             // button8
             // 
-            this.button8.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(145, 369);
+            this.button8.Location = new System.Drawing.Point(211, 176);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(42, 38);
-            this.button8.TabIndex = 8;
-            this.button8.Text = "8";
+            this.button8.Size = new System.Drawing.Size(42, 43);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "1";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
-            this.button7.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(309, 399);
+            this.button7.Location = new System.Drawing.Point(336, 238);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(42, 38);
-            this.button7.TabIndex = 7;
-            this.button7.Text = "7";
+            this.button7.Size = new System.Drawing.Size(42, 43);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "1";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            this.button6.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(477, 304);
+            this.button6.Location = new System.Drawing.Point(271, 428);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(42, 38);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "6";
+            this.button6.Size = new System.Drawing.Size(42, 43);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "1";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(298, 232);
+            this.button5.Location = new System.Drawing.Point(462, 362);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(42, 38);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "5";
+            this.button5.Size = new System.Drawing.Size(42, 43);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "1";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(168, 191);
+            this.button4.Location = new System.Drawing.Point(536, 201);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(42, 38);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "4";
+            this.button4.Size = new System.Drawing.Size(42, 43);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "1";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(136, 49);
+            this.button3.Location = new System.Drawing.Point(412, 83);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(42, 38);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "0";
+            this.button3.Size = new System.Drawing.Size(42, 43);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "1";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(67, 120);
+            this.button2.Location = new System.Drawing.Point(226, 36);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(42, 38);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "3";
+            this.button2.Size = new System.Drawing.Size(42, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "1";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(423, 152);
+            this.button1.Location = new System.Drawing.Point(72, 63);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(42, 38);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "2";
+            this.button1.Size = new System.Drawing.Size(42, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // btn1
-            // 
-            this.btn1.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn1.Location = new System.Drawing.Point(246, 120);
-            this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(42, 38);
-            this.btn1.TabIndex = 0;
-            this.btn1.Text = "1";
-            this.btn1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Google Sans", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(718, 33);
+            this.label1.Location = new System.Drawing.Point(932, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(280, 36);
             this.label1.TabIndex = 1;
@@ -185,7 +175,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(685, 86);
+            this.label2.Location = new System.Drawing.Point(902, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(135, 26);
             this.label2.TabIndex = 2;
@@ -206,7 +196,7 @@
             "7",
             "8",
             "9"});
-            this.cbMatrixOrder.Location = new System.Drawing.Point(826, 83);
+            this.cbMatrixOrder.Location = new System.Drawing.Point(1043, 90);
             this.cbMatrixOrder.Name = "cbMatrixOrder";
             this.cbMatrixOrder.Size = new System.Drawing.Size(79, 34);
             this.cbMatrixOrder.TabIndex = 3;
@@ -218,7 +208,7 @@
             this.panel2.Controls.Add(this.btnDrawSpaningTree);
             this.panel2.Controls.Add(this.cbStartNode);
             this.panel2.Controls.Add(this.label3);
-            this.panel2.Location = new System.Drawing.Point(663, 144);
+            this.panel2.Location = new System.Drawing.Point(877, 145);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(385, 138);
             this.panel2.TabIndex = 1;
@@ -267,18 +257,18 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.panel1.Location = new System.Drawing.Point(663, 288);
+            this.panel1.Location = new System.Drawing.Point(877, 289);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(385, 253);
+            this.panel1.Size = new System.Drawing.Size(385, 320);
             this.panel1.TabIndex = 4;
             // 
             // btnImportMatrix
             // 
             this.btnImportMatrix.BackColor = System.Drawing.SystemColors.ControlDark;
             this.btnImportMatrix.Font = new System.Drawing.Font("Google Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnImportMatrix.Location = new System.Drawing.Point(930, 83);
+            this.btnImportMatrix.Location = new System.Drawing.Point(1144, 73);
             this.btnImportMatrix.Name = "btnImportMatrix";
-            this.btnImportMatrix.Size = new System.Drawing.Size(106, 34);
+            this.btnImportMatrix.Size = new System.Drawing.Size(106, 66);
             this.btnImportMatrix.TabIndex = 9;
             this.btnImportMatrix.Text = "Nhập";
             this.btnImportMatrix.UseVisualStyleBackColor = false;
@@ -288,7 +278,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1060, 553);
+            this.ClientSize = new System.Drawing.Size(1274, 621);
             this.Controls.Add(this.btnImportMatrix);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
@@ -317,7 +307,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnDrawSpaningTree;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnImportMatrix;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button7;
@@ -327,7 +317,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button btnImportMatrix;
+        private System.Windows.Forms.Button button10;
     }
 }
 
--- BFS_visualization/Form1.cs
+++ BFS_visualization/Form1.cs
@@ -12,22 +12,53 @@ namespace BFS_visualization
 {
     public partial class Form1 : Form
     {
+        List<Button> buttons = new List<Button>();
+        public List<Point> points = new List<Point>();
+       
         public Form1()
         {
             InitializeComponent();
-
+            initializePoints();
         }
 
         void DrawTree(string order)
         {
-            if(order == "2")
+            int[,] A = new int[10, 10] {
+                                    { 0, 1, 0, 1, 0, 0, 0, 0, 0, 0 },
+                                    { 1, 0, 1, 0, 1, 1, 0, 0, 0, 0 },
+                                    { 0, 1, 0, 0, 0, 0, 1, 0, 0, 0 },
+                                    { 1, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+                                    { 0, 1, 0, 1, 0, 0, 0, 0, 0, 1 },
+                                    { 0, 1, 0, 0, 0, 0, 1, 0, 0, 1 },
+                                    { 0, 0, 1, 0, 0, 1, 0, 1, 0, 0 },
+                                    { 0, 0, 0, 0, 0, 0, 1, 0, 1, 0 },
+                                    { 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
+                                    { 0, 0, 0, 0, 1, 1, 0, 0, 1, 0 }
+            };
+
+            pnlGraph1.Controls.Clear();
+            pnlGraph1.Refresh();
+            Graphics gra = this.pnlGraph1.CreateGraphics();
+            Pen pen = new Pen(Color.Red, 3);
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
             {
-                pnlGraph1.Visible = false;
+                Button btn = new Button();
+                btn.Size = new Size(30, 30);
+                btn.Location = points[i];
+                btn.Text = i.ToString();
+                buttons.Add(btn);
+                pnlGraph1.Controls.Add(btn);
             }
-            else
-            {
-                pnlGraph1.Visible=true;
-            }
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    if (A[i,j] == 1)
+                    {
+                        gra.DrawLine(pen, new Point(points[i].X + 30 / 2, points[i].Y + 30 / 2), 
+                                            new Point(points[j].X + 30 / 2, points[j].Y + 30 / 2));
+                    }
+                }
         }
 
         private void btnDrawSpaningTree_Click(object sender, EventArgs e)
@@ -38,12 +69,26 @@ namespace BFS_visualization
         private void cbMatrixOrder_SelectedValueChanged(object sender, EventArgs e)
         {
             DrawTree(cbMatrixOrder.SelectedIndex.ToString());
-            
         }
 
         private void btnImportMatrix_Click(object sender, EventArgs e)
         {
 
+            DrawTree("");
+        }
+
+        void initializePoints()
+        {
+            points.Add(new Point(226, 36));
+            points.Add(new Point(412, 83));
+            points.Add(new Point(536, 201));
+            points.Add(new Point(72, 63));
+            points.Add(new Point(211, 176));
+            points.Add(new Point(336, 238));
+            points.Add(new Point(462, 362));
+            points.Add(new Point(271, 428));
+            points.Add(new Point(115, 346));
+            points.Add(new Point(95, 217));
         }
     }
 }
